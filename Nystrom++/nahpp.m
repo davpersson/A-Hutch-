c48b645b40567a54d3d1b0321f498ed99410f3ca
David Persson <persson@adminosxs-MacBook-Pro.local>
2022-02-04 12:44:15 +0100

    new results for nah++

--- Nystrom++/nahpp.m
+++ Nystrom++/nahpp.m
@@ -1,13 +1,12 @@
-function [trest,cond_no] = nahpp(n,Afun,m)
+function [trest] = nahpp(n,Afun,m)
 
 S = randn(n,round(m/4)); R = randn(n,round(m/2)); G = randn(n,round(m/4));
 
 Z = Afun(R); W = Afun(S);
 
-M = S'*Z;
-cond_no = cond(M);
+M = pinv(S'*Z);
 
-trest = trace(M\(W'*Z)) + 4*(trace(G'*Afun(G)) - trace((G'*Z)*(M\(W'*G))))/m;
+trest = trace(M*(W'*Z)) + 4*(trace(G'*Afun(G)) - trace((G'*Z)*(M*(W'*G))))/m;
 %appr_err = trace(Afun(W)*(M'\Z'))+trace(Afun(Z)*(M\W'))-norm(Z*(M\W'),'fro')^2;
 
 end
